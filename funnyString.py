4dadfada7fe62010acb3c3685ff60729c453b0d6
singhanat <ainghast@gmail.com>
2025-03-01 23:23:48 +0700

    first commit

---
+++ funnyString.py
@@ -0,0 +1,10 @@
+def funnyString(s):
+    sr = s[::-1]
+    for i in range(1,len(s)):
+        if abs(ord(s[i])-ord(s[i-1])) != abs(ord(sr[i])-ord(sr[i-1])):
+            return 'Not Funny'
+    return 'Funny'
+
+
+# acxz    
+# bcxz
